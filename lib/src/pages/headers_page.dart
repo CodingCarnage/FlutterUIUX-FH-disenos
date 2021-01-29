67f8baacde1f4f1b11b1b8115d427ac92fc630cf
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-01-29 17:25:37 -0600

    Se creo una pagina.

---
+++ lib/src/pages/headers_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'package:disenos/src/widgets/headers_widget.dart';
+
+class HeadersPage extends StatelessWidget {
+  const HeadersPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: HeaderCircularBorders(),
+    );
+  }
+}
