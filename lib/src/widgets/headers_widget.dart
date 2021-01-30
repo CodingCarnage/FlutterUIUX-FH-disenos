294d5cf9704350ca37dfb18551c61b3ac10bf834
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-01-29 18:07:30 -0600

    Se creo un header con una diagonal con un custom painter.

--- lib/src/widgets/headers_widget.dart
+++ lib/src/widgets/headers_widget.dart
@@ -26,10 +26,55 @@ class HeaderCircularBorders extends StatelessWidget {
       decoration: BoxDecoration(
         color: Color(0xff615AAB),
         borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(50.0),
-          bottomRight: Radius.circular(50.0),
+          bottomLeft: Radius.circular(70.0),
+          bottomRight: Radius.circular(70.0),
         ),
       ),
     );
   }
+}
+
+class HeaderDiagonal extends StatelessWidget {
+  const HeaderDiagonal({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size deviceSize = MediaQuery.of(context).size;
+
+    return Container(
+      height: deviceSize.height * 0.35,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderDiagonalPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderDiagonalPainter extends CustomPainter{
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint();
+
+    // Paint Properties
+    paint.color = Color(0xff615AAB);
+    paint.style = PaintingStyle.fill;
+
+    final Path path = new Path();
+
+    // Draw
+    path.moveTo(0, size.height);
+    path.lineTo(size.width, (size.height - 20.0));
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+    path.close();
+
+    canvas.drawPath(path, paint);
+  }
+  
+    @override
+    bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+
 }
