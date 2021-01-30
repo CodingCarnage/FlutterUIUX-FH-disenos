b172b6bf276963e9bfdaf1aced840eb43042bd2b
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-01-29 18:45:41 -0600

    Se creo un header circular.

--- lib/src/widgets/headers_widget.dart
+++ lib/src/widgets/headers_widget.dart
@@ -76,8 +76,48 @@ class _HeaderDiagonalPainter extends CustomPainter{
     bool shouldRepaint(covariant CustomPainter oldDelegate) {
     return true;
   }
-
 }
+class HeaderTriangle extends StatelessWidget {
+  const HeaderTriangle({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderTrianglePainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderTrianglePainter extends CustomPainter{
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint();
+
+    // Paint Properties
+    paint.color = Color(0xff615AAB);
+    paint.style = PaintingStyle.fill;
+
+    final Path path = new Path();
+
+    // Draw
+    path.moveTo(0, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+
+    canvas.drawPath(path, paint);
+  }
+  
+    @override
+    bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
 class HeaderPeak extends StatelessWidget {
   const HeaderPeak({Key key}) : super(key: key);
 
@@ -120,10 +160,10 @@ class _HeaderPeakPainter extends CustomPainter{
     bool shouldRepaint(covariant CustomPainter oldDelegate) {
     return true;
   }
-
 }
-class HeaderTriangle extends StatelessWidget {
-  const HeaderTriangle({Key key}) : super(key: key);
+
+class HeaderCircular extends StatelessWidget {
+  const HeaderCircular({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -131,13 +171,13 @@ class HeaderTriangle extends StatelessWidget {
       height: double.infinity,
       width: double.infinity,
       child: CustomPaint(
-        painter: _HeaderTrianglePainter(),
+        painter: _HeaderCircularPainter(),
       ),
     );
   }
 }
 
-class _HeaderTrianglePainter extends CustomPainter{
+class _HeaderCircularPainter extends CustomPainter{
   @override
   void paint(Canvas canvas, Size size) {
     final Paint paint = Paint();
@@ -145,13 +185,15 @@ class _HeaderTrianglePainter extends CustomPainter{
     // Paint Properties
     paint.color = Color(0xff615AAB);
     paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 10.0;
 
     final Path path = new Path();
 
     // Draw
     path.moveTo(0, 0);
-    path.lineTo(size.width, size.height);
-    path.lineTo(0, size.height);
+    path.lineTo(0, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.5, size.height * 0.40, size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
     path.close();
 
     canvas.drawPath(path, paint);
@@ -161,5 +203,4 @@ class _HeaderTrianglePainter extends CustomPainter{
     bool shouldRepaint(covariant CustomPainter oldDelegate) {
     return true;
   }
-
 }
