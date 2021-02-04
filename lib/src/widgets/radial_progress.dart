53a86a478a8f45c866475c5e2664c51347ec66cc
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-02-04 13:43:12 -0600

    Se agrego el custompainter al widget personalizado.

--- lib/src/widgets/radial_progress.dart
+++ lib/src/widgets/radial_progress.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as Math;
+
 import 'package:flutter/material.dart';
 
 class RadialProgress extends StatefulWidget {
@@ -16,7 +18,57 @@ class _RadialProgressState extends State<RadialProgress> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('${ widget.percentage }', style: Theme.of(context).textTheme.headline3,),
+      padding: const EdgeInsets.all(10.0),
+      width: double.infinity,
+      height: double.infinity,
+      child: CustomPaint(
+        painter: _RadialProgressPainter(
+          percentage: widget.percentage,
+        ),
+      ),
     );
+  }
+}
+
+class _RadialProgressPainter extends CustomPainter {
+  _RadialProgressPainter({
+    this.percentage,
+  });
+
+  final percentage;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    //* Complete circle.
+    final Paint paint = new Paint()
+      ..strokeWidth = 4
+      ..color = Colors.grey
+      ..style = PaintingStyle.stroke;
+    final Offset center = new Offset(size.width / 2, size.height / 2);
+    final double radius = Math.min(size.width / 2, size.height / 2);
+
+    canvas.drawCircle(center, radius, paint);
+
+    //* Arch
+    final Paint paintArc = new Paint()
+      ..strokeWidth = 10
+      ..color = Colors.pinkAccent
+      ..style = PaintingStyle.stroke;
+
+    //* Parts to fill.
+    double arcAngle = 2 * Math.pi * (percentage / 100);
+
+    canvas.drawArc(
+      Rect.fromCircle(center: center, radius: radius),
+      -Math.pi / 2,
+      arcAngle,
+      false,
+      paintArc,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
