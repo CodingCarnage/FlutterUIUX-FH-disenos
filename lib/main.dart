7318aab8b4ea7afb29cb47c4e0f9fdcb4aeae6a3
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-02-03 19:27:31 -0600

    Se cambio la carpeta del archivo

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 // import 'package:disenos/src/pages/headers_page.dart';
 // import 'package:disenos/src/pages/animations_page.dart';
 // import 'package:disenos/src/challenges/square_animated_page.dart';
-import 'package:disenos/src/pages/circular_progress_page.dart';
+import 'package:disenos/src/labs/circular_progress_page.dart';
  
 void main() => runApp(MyApp());
  
