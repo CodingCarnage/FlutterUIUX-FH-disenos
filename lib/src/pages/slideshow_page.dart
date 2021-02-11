8cf53080873b76cd6a4a734dadb43acb6ea54138
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-02-10 20:01:03 -0600

    Se agregaron puntos para poder visualizar donde me encuentro en el Slide.

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -9,7 +9,12 @@ class SlideShowPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: _Slides(),
+        child: Column(
+          children: <Widget>[
+            Expanded(child: _Slides()),
+            _Dots(),
+          ],
+        ),
       ),
     );
   }
@@ -45,8 +50,51 @@ class _Slide extends StatelessWidget {
     return Container(
       width: double.infinity,
       height: double.infinity,
-      padding: const EdgeInsets.all(30),
+      padding: const EdgeInsets.all(30.0),
       child: SvgPicture.asset(svg),
+    );
+  }
+}
+
+class _Dots extends StatelessWidget {
+  const _Dots({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+    return Container(
+      width: double.infinity,
+      height: screenSize.height * 0.05,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          _Dot(screenSize: screenSize),
+          _Dot(screenSize: screenSize),
+          _Dot(screenSize: screenSize),
+        ],
+      ),
+    );
+  }
+}
+
+class _Dot extends StatelessWidget {
+  const _Dot({
+    Key key,
+    @required this.screenSize,
+  }) : super(key: key);
+
+  final Size screenSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: screenSize.height * 0.015,
+      width: screenSize.height * 0.015,
+      margin: const EdgeInsets.symmetric(horizontal: 5.0),
+      decoration: BoxDecoration(
+        color: Colors.orange,
+        shape: BoxShape.circle,
+      ),
     );
   }
 }
