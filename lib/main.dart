e20f741a7d5614b43ddef4959915572d0302dd3c
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-02-15 14:02:56 -0600

    Se cambio el home.

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,8 @@ import 'package:flutter/material.dart';
 // import 'package:disenos/src/labs/circular_progress_page.dart';
 // import 'package:disenos/src/pages/pie_charts_page.dart';
 // import 'package:disenos/src/labs/slideshow_page.dart';
-import 'package:disenos/src/pages/slideshow_page.dart';
+// import 'package:disenos/src/pages/slideshow_page.dart';
+import 'package:disenos/src/pages/pinterest_page.dart';
  
 void main() => runApp(MyApp());
  
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños App',
-      home: SlideshowPage(),
+      home: PinterestPage(),
     );
   }
 }
