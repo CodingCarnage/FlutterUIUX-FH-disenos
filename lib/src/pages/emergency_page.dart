88362600a82172de10280026cf3ee8c1fc7125c8
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-02-18 16:37:49 -0600

    Se agrego un IconHeader en el Scaffold.

--- lib/src/pages/emergency_page.dart
+++ lib/src/pages/emergency_page.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'package:disenos/src/widgets/headers_widget.dart';
+
 class EmergencyPage extends StatelessWidget {
   const EmergencyPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Emergency Page'),
-      ),
+      body: IconHeader(),
     );
   }
 }
