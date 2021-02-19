15dfe09e9e0d6df50ee06676c5f0937f2defe868
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-02-18 18:09:31 -0600

    Se asignaron valores a los parametros del IconHeader.

--- lib/src/pages/emergency_page.dart
+++ lib/src/pages/emergency_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:disenos/src/widgets/headers_widget.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class EmergencyPage extends StatelessWidget {
   const EmergencyPage({Key key}) : super(key: key);
@@ -8,7 +9,15 @@ class EmergencyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IconHeader(),
+      body: IconHeader(
+        icon: FontAwesomeIcons.pizzaSlice,
+        title: 'Pizza Klein',
+        subtitle: 'Blvd. Francisco I Madero, Guadalupe',
+        backgroundColors: <Color>[
+          Color(0xffC93231),
+          Color(0xff8F3D7D),
+        ],
+      ),
     );
   }
 }
