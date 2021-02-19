7605e2c97021b3919780039e9577cba1d8be4988
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-02-18 19:16:17 -0600

    Se personalizo aun mas el widget.

--- lib/src/widgets/large_button_widget.dart
+++ lib/src/widgets/large_button_widget.dart
@@ -1,11 +1,52 @@
 import 'package:flutter/material.dart';
 
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 class LargeButton extends StatelessWidget {
   const LargeButton({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return _LargeButtonBackground();
+    return Stack(
+      children: <Widget>[
+        _LargeButtonBackground(),
+        _LargeButtonForeground(),
+      ],
+    );
+  }
+}
+
+class _LargeButtonForeground extends StatelessWidget {
+  const _LargeButtonForeground({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+
+    return Container(
+      width: double.infinity,
+      height: screenSize.height * 0.1,
+      margin: const EdgeInsets.all(20.0),
+      child: Center(
+        child: ListTile(
+          leading: FaIcon(
+            FontAwesomeIcons.carCrash,
+            color: Colors.white,
+            size: screenSize.width * 0.1,
+          ),
+          title: Text(
+            'Car accident',
+            style: Theme.of(context).textTheme.headline6.copyWith(color: Colors.white),
+          ),
+          trailing: FaIcon(
+            FontAwesomeIcons.chevronRight,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
   }
 }
 
@@ -17,12 +58,29 @@ class _LargeButtonBackground extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Size screenSize = MediaQuery.of(context).size;
+    
     return Container(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15.0),
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              right: -20.5,
+              top: -20.5,
+              child: FaIcon(
+                FontAwesomeIcons.carCrash,
+                size: screenSize.width * 0.325,
+                color: Colors.white.withOpacity(0.2),
+              ),
+            ),
+          ],
+        ),
+      ),
       width: double.infinity,
       height: screenSize.height * 0.1,
       margin: const EdgeInsets.all(20.0),
       decoration: BoxDecoration(
-        color: Colors.red,
+        borderRadius: BorderRadius.circular(15.0),
         boxShadow: <BoxShadow>[
           BoxShadow(
             color: Colors.black.withOpacity(0.2),
@@ -30,12 +88,11 @@ class _LargeButtonBackground extends StatelessWidget {
             blurRadius: 10.0,
           )
         ],
-        borderRadius: BorderRadius.circular(15.0),
         gradient: LinearGradient(
           colors: <Color>[
             Color(0xff54C5F8),
             Color(0xff01579B),
-          ]
+          ],
         ),
       ),
     );
