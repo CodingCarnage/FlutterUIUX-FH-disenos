5e532efba76056e01d8d24604a298ccd45616051
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-02-18 18:28:53 -0600

    Se agrego el nuevo widget al scaffold.

--- lib/src/pages/emergency_page.dart
+++ lib/src/pages/emergency_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
-import 'package:disenos/src/widgets/headers_widget.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'package:disenos/src/widgets/headers_widget.dart';
+import 'package:disenos/src/widgets/large_button_widget.dart';
 
 class EmergencyPage extends StatelessWidget {
   const EmergencyPage({Key key}) : super(key: key);
@@ -9,15 +11,28 @@ class EmergencyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IconHeader(
-        icon: FontAwesomeIcons.pizzaSlice,
-        title: 'Pizza Klein',
-        subtitle: 'Blvd. Francisco I Madero, Guadalupe',
-        backgroundColors: <Color>[
-          Color(0xffC93231),
-          Color(0xff8F3D7D),
-        ],
+      body: Center(
+        child: LargeButton(),
       ),
+    );
+  }
+}
+
+class _PageHeader extends StatelessWidget {
+  const _PageHeader({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconHeader(
+      icon: FontAwesomeIcons.pizzaSlice,
+      title: 'Pizza Klein',
+      subtitle: 'Blvd. Francisco I Madero, Guadalupe',
+      backgroundColors: <Color>[
+        Color(0xffC93231),
+        Color(0xff8F3D7D),
+      ],
     );
   }
 }
