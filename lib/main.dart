d1168b7a92651ab7bf8b14a20a6b59b55e8f2b93
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-02-24 12:59:57 -0600

    Se implemento el ChangeNotifierProvider en el mas alto nivel.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
 
-// import 'package:disenos/src/pages/headers_page.dart';
-// import 'package:disenos/src/pages/animations_page.dart';
-// import 'package:disenos/src/challenges/square_animated_page.dart';
-// import 'package:disenos/src/labs/circular_progress_page.dart';
-// import 'package:disenos/src/pages/pie_charts_page.dart';
-// import 'package:disenos/src/labs/slideshow_page.dart';
-// import 'package:disenos/src/pages/slideshow_page.dart';
-// import 'package:disenos/src/pages/pinterest_page.dart';
-// import 'package:disenos/src/pages/emergency_page.dart';
-// import 'package:disenos/src/pages/sliver_list_page.dart';
+import 'package:provider/provider.dart';
+
 import 'package:disenos/src/pages/launcher_page.dart';
- 
-void main() => runApp(MyApp());
- 
+
+import 'package:disenos/src/theme/theme.dart';
+
+void main() => runApp(
+      ChangeNotifierProvider(
+        create: (_) => ThemeChanger(),
+        child: MyApp(),
+      ),
+    );
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
